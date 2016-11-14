2703a392cb1adff1766b02b76284052ce6392837
shaymark <shaymark@gmail.com>
2016-11-14 14:36:11 +0200

    add collection screen

---
+++ ItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCollectionViewCell.swift
+//  exercises1
+//
+//  Created by shay markovich on 14/11/2016.
+//  Copyright © 2016 shay markovich. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}

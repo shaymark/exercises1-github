85a8cf3f003ac9f922eeb25f1c4eca2e36e3d40e
shaymark <shaymark@gmail.com>
2016-11-15 23:52:48 +0200

    missing files

---
+++ Pods/Target Support Files/PKRevealController/PKRevealController-umbrella.h
@@ -0,0 +1,19 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "CAAnimation+PKIdentifier.h"
+#import "CALayer+PKConvenienceAnimations.h"
+#import "NSObject+PKBlocks.h"
+#import "UIViewController+PKRevealController.h"
+#import "PKRevealControllerView.h"
+#import "PKAnimating.h"
+#import "PKAnimation.h"
+#import "PKLayerAnimator.h"
+#import "PKSequentialAnimation.h"
+#import "PKLog.h"
+#import "PKRevealController.h"
+
+FOUNDATION_EXPORT double PKRevealControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char PKRevealControllerVersionString[];
+
